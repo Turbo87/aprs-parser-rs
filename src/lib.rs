32ff94309c11f3c7668be4af6fbeb851bf2c6ff4
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-05-24 00:36:18 +0200

    Add basic `parse()` function

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,14 @@ mod message;
 mod position;
 mod timestamp;
 
+use std::str::FromStr;
+
 pub use error::APRSError;
 pub use callsign::Callsign;
 pub use message::APRSMessage;
 pub use position::APRSPosition;
 pub use timestamp::Timestamp;
+
+pub fn parse(s: &str) -> Result<APRSMessage, APRSError> {
+    APRSMessage::from_str(s)
+}
